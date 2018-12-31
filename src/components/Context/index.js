9385c6403b677be30b09207ce4e6616edcde5424
Jordan Mor <jordanmor@gmail.com>
2018-12-30 19:17:32 -0500

    Use Wordnik API to get random word for Flickr tag search

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -9,11 +9,24 @@ export class Provider extends Component {
   }
 
   componentDidMount() {
-    this.getImages('dogs');
+    this.getImages();
   }
 
-  getImages = async(tag) => {
+  getRandomWord = async() => {
+    const wordnikApiKey = process.env.REACT_APP_WORDNIK_APIKEY;
+    const response = await axios.get(`https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun&minCorpusCount=1000&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=-1&api_key=${wordnikApiKey}`);
+    if(response.status === 200) {
+      const { data } = response;
+      return data.word;
+    } else {
+      return;
+    }
+  }
+
+  getImages = async() => {
     const apiKey = process.env.REACT_APP_FLICKR_APIKEY;
+    const tag = await this.getRandomWord();
+    console.log(tag);
     const { data } = await axios(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=14&format=json&nojsoncallback=1`);
     const images = data.photos.photo.slice(0, 10);
     this.setState({ images });

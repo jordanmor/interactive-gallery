8ec8c90da86471aecee458c1f194f75a2291608d
Jordan Mor <jordanmor@gmail.com>
2019-01-05 19:01:43 -0500

    Updates

---
+++ precache-manifest.7e7fdaf7f08557b6ebbbf27355f5b33c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b719f07db34de9345a4",
+    "url": "/interactive-gallery/static/css/main.a4cbae90.chunk.css"
+  },
+  {
+    "revision": "4b719f07db34de9345a4",
+    "url": "/interactive-gallery/static/js/main.4b719f07.chunk.js"
+  },
+  {
+    "revision": "87599c9ac9fe834b5f80",
+    "url": "/interactive-gallery/static/js/1.87599c9a.chunk.js"
+  },
+  {
+    "revision": "7031c84e5df1449b3ffe",
+    "url": "/interactive-gallery/static/js/runtime~main.7031c84e.js"
+  },
+  {
+    "revision": "c802686bb65b2554e04b3ed1c863df70",
+    "url": "/interactive-gallery/index.html"
+  }
+];
